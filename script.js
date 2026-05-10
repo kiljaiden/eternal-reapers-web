365f2d45af0932f06daecaff58a840dad7a87112
KilJaiden <73482746+kiljaiden@users.noreply.github.com>
2026-05-09 22:01:51 -0500

    Update Firebase configuration with new credentials

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    databaseURL: "TU_DATABASE_URL",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyDGELmVvBZZywbJOf6kuVKQJjKaRt6Q9ws",
+  authDomain: "eternalreapersweb.firebaseapp.com",
+  databaseURL: "https://eternalreapersweb-default-rtdb.firebaseio.com",
+  projectId: "eternalreapersweb",
+  storageBucket: "eternalreapersweb.firebasestorage.app",
+  messagingSenderId: "945508907683",
+  appId: "1:945508907683:web:b26cc3660298d4d6ba1d16",
+  measurementId: "G-7J20NR0JDM"
 };
 
 firebase.initializeApp(firebaseConfig);
