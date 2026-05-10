5aff5c0fb1623f34a69f702c7ff37c7e3204fa21
KilJaiden <73482746+kiljaiden@users.noreply.github.com>
2026-05-09 22:02:18 -0500

    Add Firebase initialization to script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyDGELmVvBZZywbJOf6kuVKQJjKaRt6Q9ws",
   authDomain: "eternalreapersweb.firebaseapp.com",
